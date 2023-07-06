0894aa62cbdc50f3b9654738e04c130e79f7776d
Dash <dkshowarth@gmail.com>
2023-07-06 15:59:06 -0700

    fix issue with js pointers returning null

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
-var timeLeft = document.getElementById("#time");
-var question = document.getElementById("#question");
-var initials = document.getElementById("#initials");
-var submitScore = document.getElementById("#submit");
-var newGame = document.getElementById("#newGame")
-var guessTrue = document.getElementById("#isTrue");
-var guessFalse = document.getElementById("#isFalse");
-var guessFeedback = document.getElementById("#guessFeedback");
+var timeLeft = document.getElementById("time");
+var question = document.getElementById("question");
+var initials = document.getElementById("initials");
+var submitScore = document.getElementById("submitScore");
+var newGame = document.getElementById("newGame")
+var guessTrue = document.getElementById("isTrue");
+var guessFalse = document.getElementById("isFalse");
+var guessFeedback = document.getElementById("guessFeedback");
 var finalScore = 0;
 
 submitScore.addEventListener("click", function(event){
     event.preventDefault();
-    alert("we made it into function");
     // 
     localStorage.setItem(initials, JSON.stringify(finalScore));
 })
